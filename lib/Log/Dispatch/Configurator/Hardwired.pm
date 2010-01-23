80b387dc18a2780bdfc706698595bc70bf6f2dbf
James S White <james@jameswhite.org>
2010-01-23 11:32:33 -0600

    added hardwired code

--- lib/Log/Dispatch/Configurator/Hardwired.pm
+++ lib/Log/Dispatch/Configurator/Hardwired.pm
@@ -27,7 +27,38 @@ our @EXPORT = qw(
 
 our $VERSION = '0.01';
 
+use base qw(Log::Dispatch::Configurator);
 
+sub new{
+    my $class=shift;
+    my $self = {};
+    bless $self;
+    my $construct = shift if @_;
+    foreach my $key (keys(%{ $construct })){
+        push (@{ $self->{'dispatchers'} },$key);
+        $self->{'attrs'}->{$key} = $construct->{$key};
+    }
+    #print Data::Dumper->Dump([$self]);
+    return $self; 
+}
+
+sub get_attrs_global {
+    my $self = shift;
+    return {
+             format => undef,
+             dispatchers => $self->{'dispatchers'}
+           };
+}
+
+sub get_attrs {
+    my($self, $name) = @_;
+    if (defined $self->{'attrs'}->{$name}) {
+        return $self->{'attrs'}->{$name};
+    }else{
+        warn "invalid dispatcher name: $name";
+    }
+}
+sub needs_reload { 1 }
 # Preloaded methods go here.
 
 1;
